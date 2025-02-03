62c083816304488beb0d15c2679d65942483925c
Marina.S. <marina.sevostian@gmail.com>
2025-02-03 05:44:34 +0200

    add init

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,11 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store, { persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
